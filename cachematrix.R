9bbf901c0d25647e6d83e92b9d57747013a893b8
German Araujo <araujomelogno@gmail.com>
2015-05-20 00:46:18 -0300

    initial version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script has two functions similar to the "makemeanVector" example of 
+## the course.
 
-## Write a short comment describing this function
+## makeCaheMatrix creates the object that keep information of 
+##  QueryMatrix (x) : is the matrix that we want to inverse
+##  InverseMatrix (m) : is the inverse of the matrix saved in the cache
+##  OriginalMatrix (original) : is original matrix we from which we built 
+##                              the InvereMAtrix(m).used  to compare 
+##                              (when calling the function the nex time)
+##                              if the matrix we want to inverse has not 
+##                              changed
+## the nested functions are getters and setters of the attributes
 
+## cacheSolve a cacheMatrix object (created by mackeCahceMAtrix function)
+## and if we have already calculated the inverse of the queryMatrix
+## (and this matrix has not changed -identical to originalMatrix- )
+## the inverse cached matrix is returned 
+## otherwise we calculate the inverse of the queryMatrix and save
+## the result and the query matrix as the original matrix 
+
+
+
+## makeCaheMatrix creates the object that keep information of 
+##  QueryMatrix (x) : is the matrix that we want to inverse
+##  InverseMatrix (m) : is the inverse of the matrix saved in the cache
+##  OriginalMatrix (original) : is original matrix we from which we built 
+##                              the InvereMAtrix(m).used  to compare 
+##                              (when calling the function the nex time)
+##                              if the matrix we want to inverse has not 
+##                              changed
+## the nested functions are getters and setters of the attributes
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  original <- NULL
+  setQueryMatrix <- function(y) {
+    x <<- y
+    m <<- NULL
+    original <<- NULL
+  }
+  getQueryMatrix <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  setOriginalMatrix <- function(om) original <<- om
+  getOriginalMatrix <- function() original
+  list(
+       setOriginalMatrix = setOriginalMatrix, 
+       getOriginalMatrix = getOriginalMatrix,
+       setQueryMatrix = setQueryMatrix, 
+       getQueryMatrix = getQueryMatrix,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve a cacheMatrix object (created by mackeCahceMAtrix function)
+## and if we have already calculated the inverse of the queryMatrix
+## (and this matrix has not changed -identical to originalMatrix- )
+## the inverse cached matrix is returned 
+## otherwise we calculate the inverse of the queryMatrix and save
+## the result and the query matrix as the original matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverseMatrix <- x$getInverse()
+  ##if the inverse of  matrix queried  is cached we must be sure that 
+  ## the matrix queried has not changed (must be identical to the original matrix)
+  if(!is.null(inverseMatrix) && identical(x$getQueryMatrix(),x$getOriginalMatrix())) {
+    return(inverseMatrix)
+  }
+  queryMatrix <- x$getQueryMatrix()
+  inverseMatrix <- solve(queryMatrix)
+  x$setInverse(inverseMatrix)
+  x$setOriginalMatrix(queryMatrix)
+  inverseMatrix
 }
